45ca024f4035961c1eb2bcf0ca5bcf778fa85c10
Changhyun Yoo <chyoo0428@gmail.com>
2022-04-20 00:05:12 +0000

    revised based on the feedback!

--- js/main.js
+++ js/main.js
@@ -4,40 +4,44 @@ function comingMovie() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://imdb-api.com/en/API/ComingSoon/k_4003h2lv');
   xhr.responseType = 'json';
-  xhr.addEventListener('load', function (event) {
+  xhr.addEventListener('load', function () {
     var items = xhr.response.items;
     for (var i = 0; i < items.length; i++) {
-      var $li = document.createElement('li');
-
-      var $briefCon = document.createElement('div');
-      $briefCon.setAttribute('class', 'brief-container');
-      $li.append($briefCon);
-
-      var $briefImg = document.createElement('div');
-      $briefImg.setAttribute('class', 'image-container');
-      $briefCon.appendChild($briefImg);
-
-      var $img = document.createElement('img');
-      $img.setAttribute('src', items[i].image);
-      $img.setAttribute('class', 'brief-image');
-      $briefImg.appendChild($img);
-
-      var $briefDesc = document.createElement('div');
-      $briefDesc.setAttribute('class', 'brief-description text-align-center');
-      $briefCon.append($briefDesc);
-
-      var $title = document.createElement('p');
-      $title.textContent = items[i].title;
-      $briefDesc.append($title);
-
-      var $year = document.createElement('p');
-      $year.textContent = items[i].year;
-      $briefDesc.append($year);
-
-      $ul.append($li);
+      var description = movieDescription(items[i]);
+      $ul.append(description);
     }
   });
   xhr.send();
 }
-
 comingMovie();
+
+function movieDescription(object) {
+  var $li = document.createElement('li');
+
+  var $briefCon = document.createElement('div');
+  $briefCon.setAttribute('class', 'brief-container');
+  $li.append($briefCon);
+
+  var $briefImg = document.createElement('div');
+  $briefImg.setAttribute('class', 'image-container');
+  $briefCon.appendChild($briefImg);
+
+  var $img = document.createElement('img');
+  $img.setAttribute('src', object.image);
+  $img.setAttribute('class', 'brief-image');
+  $briefImg.appendChild($img);
+
+  var $briefDesc = document.createElement('div');
+  $briefDesc.setAttribute('class', 'brief-description text-align-center');
+  $briefCon.append($briefDesc);
+
+  var $title = document.createElement('p');
+  $title.textContent = object.title;
+  $briefDesc.append($title);
+
+  var $year = document.createElement('p');
+  $year.textContent = object.year;
+  $briefDesc.append($year);
+
+  return $li;
+}
