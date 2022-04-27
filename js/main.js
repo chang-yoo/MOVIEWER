1354702ba927ee920c9affb895a36134c1423208
Changhyun Yoo <chyoo0428@gmail.com>
2022-04-27 19:41:24 +0000

    removed unneccesary files and directories

--- js/main.js
+++ js/main.js
@@ -17,6 +17,8 @@ window.addEventListener('load', comingMovie);
 
 function movieDescription(object) {
   var $li = document.createElement('li');
+  $li.setAttribute('class', 'descriptions');
+  $li.setAttribute('data-id', object.id);
 
   var $briefCon = document.createElement('div');
   $briefCon.setAttribute('class', 'brief-container');
@@ -37,28 +39,40 @@ function movieDescription(object) {
 
   var $anchor = document.createElement('a');
   $anchor.setAttribute('href', '#');
+  $anchor.setAttribute('class', 'brief-text-box');
   $briefDesc.append($anchor);
 
   var $title = document.createElement('p');
   $title.setAttribute('data-id', object.id);
-  $title.setAttribute('class', 'detailed-title');
+  $title.setAttribute('class', 'brief-title');
   $title.textContent = object.title;
   $anchor.append($title);
 
   var $year = document.createElement('h6');
   $year.setAttribute('data-id', object.id);
-  $year.setAttribute('class', 'detailed-year');
+  $year.setAttribute('class', 'brief-year');
   $year.textContent = object.year;
   $anchor.append($year);
+
+  var $deleteBox = document.createElement('div');
+  $deleteBox.setAttribute('class', 'delete-button hidden');
+  $briefDesc.append($deleteBox);
+
+  var $delete = document.createElement('a');
+  $delete.setAttribute('class', 'delete');
+  $delete.textContent = 'Delete';
+  $deleteBox.append($delete);
+
   return $li;
 }
 
 function detailsMovie(object) {
   var $row = document.createElement('div');
   $row.setAttribute('class', 'row details');
+  $row.setAttribute('data-id', object.id);
 
   var $detailImgCont = document.createElement('div');
-  $detailImgCont.setAttribute('class', 'detailed-image-container column-one-third');
+  $detailImgCont.setAttribute('class', 'detailed-image-container column-half');
   $row.append($detailImgCont);
 
   var $img = document.createElement('img');
@@ -79,8 +93,14 @@ function detailsMovie(object) {
   $descriptionBox.append($desciptionText);
 
   var $descriptionTitle = document.createElement('h4');
+  $descriptionTitle.setAttribute('class', 'brief-title');
   $descriptionTitle.textContent = 'Title: ' + object.title;
   $desciptionText.append($descriptionTitle);
+
+  var $descriptionYear = document.createElement('h4');
+  $descriptionYear.setAttribute('class', 'brief-year');
+  $descriptionYear.textContent = object.year;
+  $desciptionText.append($descriptionYear);
 
   var $descriptionReleaseDate = document.createElement('h4');
   $descriptionReleaseDate.textContent = 'Release Date: ' + object.releaseState;
@@ -126,7 +146,10 @@ function hideList() {
   $listBox.className = 'list-box hidden';
   var $details = document.querySelector('.detail-container');
   $details.setAttribute('class', 'column-full detail-container');
-  hideWatchList();
+  var $listContainer = document.querySelector('.list-container');
+  var $watchList = document.querySelector('.watch-list-container');
+  $listContainer.className = 'list-container column-full';
+  $watchList.setAttribute('class', 'watch-list-container column-full hidden');
 }
 
 function viewWatchList(event) {
@@ -153,10 +176,10 @@ function getDetails(event) {
     var text = event.target.closest('p');
     var $textIds = text.getAttribute('data-id');
     var stringId = String($textIds);
+    hideList();
   } else {
     showList();
   }
-  hideList();
   xhr.addEventListener('load', function (event) {
     var items = xhr.response.items;
     for (var i = 0; i < items.length; i++) {
@@ -173,28 +196,33 @@ function getDetails(event) {
 
 var $homeBtn = document.querySelector('.red-background');
 $homeBtn.addEventListener('click', showList);
+
 function showList() {
   var $listBox = document.querySelector('.list-box');
   $listBox.className = 'list-box';
   var $details = document.querySelector('.detail-container');
-  var $row = document.querySelector('.details');
   $details.className = 'hidden detail-container full-column';
-  for (var i = 0; i < $details.length; i++) {
-    if ($details[i] > 0) {
-      $details.removeChild($row);
-    }
-  }
   hideWatchList();
+  var $deleteBtn = document.querySelectorAll('.delete-button');
+  for (var q = 0; q < $deleteBtn.length; q++) {
+    $deleteBtn[q].className = 'hidden delete-button';
+  }
+  var $row = document.querySelector('.details');
+  if ($details.contains($row)) {
+    $details.removeChild($row);
+  }
 }
 
 var $addToList = document.querySelector('.add-to-list');
 
 $addToList.addEventListener('click', function addToWatchList(event) {
-  var $img = document.querySelector('.detailed-image');
+  var $addToListText = document.querySelector('.add-to-list');
+  var $li = $addToListText.nextElementSibling;
+  var $img = $li.querySelector('img');
   var $imgValue = $img.getAttribute('src');
-  var $title = document.querySelector('.detailed-title');
-  var $year = document.querySelector('.detailed-year');
-  var $id = $title.getAttribute('data-id');
+  var $title = $li.querySelector('.brief-title');
+  var $year = $li.querySelector('.brief-year');
+  var $id = $li.getAttribute('data-id');
   var datas = {
     image: $imgValue,
     title: $title.textContent,
@@ -222,17 +250,23 @@ $watchListBtn.addEventListener('click', watchList);
 
 function watchList(event) {
   hideList();
+  var $deleteBtn = document.querySelectorAll('.delete-button');
+  for (var q = 0; q < $deleteBtn.length; q++) {
+    $deleteBtn[q].className = 'delete-button';
+  }
   var details = document.querySelector('.detail-container');
   details.className = 'column-full detail-container hidden';
   viewWatchList();
+  emptyText();
+}
+
+function emptyText() {
   var movies = data.movie;
   var $watchListText = document.querySelector('.watch-list-text');
-  for (var i = 0; i < movies.length; i++) {
-    if (movies[i] === 0) {
-      $watchListText.className = 'watch-list-text';
-    } else {
-      $watchListText.className = 'watch-list-text hidden';
-    }
+  if (movies.length === 0) {
+    $watchListText.className = 'watch-list-text';
+  } else {
+    $watchListText.className = 'watch-list-text hidden';
   }
 }
 
@@ -244,4 +278,48 @@ function loadWatchList(event) {
     var selectedMovie = movieDescription(movies[i]);
     $ul.prepend(selectedMovie);
   }
+}
+
+window.addEventListener('click', deleteMovie);
+
+function deleteMovie(event) {
+  var $deleteButton = document.querySelectorAll('.delete');
+  for (var i = 0; i < $deleteButton.length; i++) {
+    if (event.target === $deleteButton[i]) {
+      var $deleteBackground = document.querySelector('.delete-confirm-background');
+      $deleteBackground.className = 'delete-confirm-background';
+      var $movieToBeRemoved = event.target.closest('li');
+      var $removeMovieId = $movieToBeRemoved.getAttribute('data-id');
+      data.editing = $removeMovieId;
+    }
+  }
+}
+
+var $deleteNo = document.querySelector('.delete-no');
+$deleteNo.addEventListener('click', removePopup);
+function removePopup(event) {
+  var $deleteBackground = document.querySelector('.delete-confirm-background');
+  $deleteBackground.className = 'delete-confirm-background hidden';
+}
+
+var $deleteYes = document.querySelector('.delete-yes');
+
+$deleteYes.addEventListener('click', removeMovie);
+
+function removeMovie(event) {
+  var $shadeBackground = document.querySelector('.delete-confirm-background');
+  $shadeBackground.className = 'delete-confirm-background hidden';
+  var $ul = document.querySelector('.watch-list');
+  var $li = $ul.querySelectorAll('li');
+  for (var q = 0; q < $li.length; q++) {
+    var ids = $li[q].getAttribute('data-id');
+    if (ids === data.editing) {
+      $ul.removeChild($li[q]);
+    }
+  }
+  for (var i = 0; i < data.movie.length; i++) {
+    if (data.editing === data.movie[i].id) {
+      data.movie.splice(i, 1);
+    }
+  } emptyText();
 }
